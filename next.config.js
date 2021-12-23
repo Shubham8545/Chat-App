7cc36cc45c54c4433d1f1ab0a1141b56411686c7
Shubham8545 <shubhamsinghbhu2016@gmail.com>
2021-12-23 18:46:28 +0530

    Final push

---
+++ next.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    reactStrictMode: true,
+    
+  
+    env: {
+        id:"9b517b3c-4481-45d0-9838-d12a12995bc3",
+        key: "e6b225e7-7372-4b27-9445-17a5a818ac18"
+    },
+    eslint: {
+      dirs: ['utils'],
+    },
+    
+  };
